39684f04f7fa6a417cd122b1cfaca048cd8f29b2
Aryan R.H <72511610+aryanrh@users.noreply.github.com>
2023-07-17 19:44:07 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -34,5 +34,5 @@ rm -rf $PARENT_DIR/master.tar*
 
 export PATH=~/clang-r416183b/bin:$PATH
 export PATH=~/build-tools/linux-x86:~/gas/linux-x86:$PATH
-make PLATFORM_VERSION=12 ANDROID_MAJOR_VERSION=s LLVM=1 LLVM_IAS=1 ARCH=arm64 TARGET_SOC=s5e8825 CROSS_COMPILE=~/build-tools/linux-x86/clang-r416183b/bin/aarch64-linux-gnu- s5e8825-a53xcis_defconfig
+make PLATFORM_VERSION=12 ANDROID_MAJOR_VERSION=s LLVM=1 LLVM_IAS=1 ARCH=arm64 TARGET_SOC=s5e8825 CROSS_COMPILE=~/build-tools/linux-x86/clang-r416183b/bin/aarch64-linux-gnu- lineage_a53x_defconfig
 make PLATFORM_VERSION=12 ANDROID_MAJOR_VERSION=s LLVM=1 LLVM_IAS=1 ARCH=arm64 TARGET_SOC=s5e8825 CROSS_COMPILE=~/clang-r416183b/bin/aarch64-linux-gnu- -j32
